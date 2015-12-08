b4a0473096cfca95a5497acaa77a0bef386b4361
James M. Joyce <james@stokesmcnutt.com>
2015-12-08 15:29:08 -0600

    Add missing action param

--- www/recentscontrol.js
+++ www/recentscontrol.js
@@ -3,15 +3,15 @@ var exec = require('cordova/exec');
 var RecentsControl = {
 	
 	setColor: function(colorStr) {
-		exec(null, null, 'RecentsControl', [colorStr]);
+		exec(null, null, 'RecentsControl', 'setColor', [colorStr]);
 	},
 	
 	setDescription: function(desc) {
-		exec(null, null, 'RecentsControl', [desc]);
+		exec(null, null, 'RecentsControl', 'setDescription', [desc]);
 	},
 	
 	setOptions: function(colorStr, desc) {
-		exec(null, null, 'RecentsControl', [colorStr, desc]);
+		exec(null, null, 'RecentsControl', 'setOptions', [colorStr, desc]);
 	},
 	
 };
